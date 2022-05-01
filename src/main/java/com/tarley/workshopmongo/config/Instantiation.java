bf4ceb2c1e3312f73a7145b09a3171f488546d07
TarleyRodrigues <tarley.divino@gmail.com>
2022-05-01 16:38:19 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/tarley/workshopmongo/config/Instantiation.java
+++ src/main/java/com/tarley/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.tarley.workshopmongo.domain.Post;
 import com.tarley.workshopmongo.domain.User;
 import com.tarley.workshopmongo.dto.AuthorDTO;
+import com.tarley.workshopmongo.dto.CommentDTO;
 import com.tarley.workshopmongo.repository.PostRepository;
 import com.tarley.workshopmongo.repository.UserRepository;
 
@@ -41,12 +42,26 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null,sdf.parse("21/03/2018"), "Partiu, viagem!", "Vou viajar para o DF, Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null,sdf.parse("23/04/2018"), "Bom dia!", "Acordei com muita preguiça hoje.", new AuthorDTO(maria));
+		Post post3 = new Post(null,sdf.parse("23/04/2018"), "Verdade seja dita!", "Cavalo dado, não se olha os dentes.", new AuthorDTO(tarley));
+
 		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(tarley));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		CommentDTO c4 = new CommentDTO("Para com isso, cara.", sdf.parse("23/03/2018"), new AuthorDTO(maria));
+		CommentDTO c5 = new CommentDTO("Ta chapando, Deolane?.", sdf.parse("25/03/2018"), new AuthorDTO(bob));
+
 		
-		postRepository.saveAll(Arrays.asList(post1, post2));
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().add(c3);
+		post3.getComments().addAll(Arrays.asList(c4, c5));
 		
+		postRepository.saveAll(Arrays.asList(post1, post2, post3));
+		
+		tarley.getPosts().add(post3);
 		maria.getPosts().addAll(Arrays.asList(post1,post2));
 		userRepository.save(maria);
+		userRepository.save(tarley);
 	}
 
 }
